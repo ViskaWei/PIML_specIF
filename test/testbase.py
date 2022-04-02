563a7316c19b26d03cbf2687c0cda2162592ad81
ViskaWei <swei20@jhu.edu>
2022-04-02 12:45:20 -0400

    updates

---
+++ test/testbase.py
@@ -0,0 +1,15 @@
+
+
+
+from unittest import TestCase
+
+
+class TestBase(TestCase):
+
+
+    
+    MODEL = {
+        "ResTune" : {"type": "Alex", "param": {"step": 10, "res":10000}},
+        }
+    PARAM = {"arm": "RedM"}
+    DATA  = {"wavesky": }
